fba233ef45dd5c77f32adc297b303a4c82b7aac8
Vijai Rajpal <vijai.p.rajpal@gmail.com>
2023-06-30 10:57:21 -0400

    first parse

---
+++ main.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+page = requests.get('https://www.baseball-reference.com/teams/SEA/2023.shtml')
+soup = BeautifulSoup(page.content, 'lxml')
+print(soup.prettify)
