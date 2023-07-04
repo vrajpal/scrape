eff7cb658993ab8150580d19ff39a2d278fff689
Vijai Rajpal <vijai.p.rajpal@gmail.com>
2023-07-04 13:19:59 -0400

    explore div

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import requests
 
 page = requests.get('https://www.baseball-reference.com/teams/SEA/2023.shtml')
 soup = BeautifulSoup(page.content, 'lxml')
-print(soup.find("div", {"id": "all_team_batting"}))
+all_team_batting = soup.find("div", {"id": "div_team_batting"})
+print(all_team_batting.children)
