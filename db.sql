0a257118e6f34f009e81e5a34384ebb52a8d7af5
Vijai <vijai.p.rajpal@gmail.com>
2023-08-05 16:55:56 -0400

    db script

---
+++ db.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS scrape_mlb;
+
+CREATE DATABASE scrape_mlb;
+
+USE scrape_mlb;
+
+CREATE TABLE player(
+  player_id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name varchar(255),
+  last_name varchar(255),
+  age INT,
+  team_id INT
+); 
+
+CREATE TABLE team(
+  team_id INT AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255),
+  location varchar(255)
+);
+
+CREATE TABLE stat(
+  stat_id INT AUTO_INCREMENT PRIMARY KEY,
+  value double(16,4) DEFAULT NULL,
+  player_id INT,
+  team_id INT
+);
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 page = requests.get('https://www.baseball-reference.com/teams/SEA/2023.shtml')
 soup = BeautifulSoup(page.content, 'lxml')
 all_team_batting = soup.find("table", {"id": "team_batting"}).find_all('tr')
-# rows = all_team_batting.find_all('tr');
+# rows = all_team_batting.find_all('tr')
 # the headers for the batting table in Baseball Ref, remove player as its method for grabbing name is inconsistent 
 batting = {"age", "G", "AB", "R", "H", "2B", "3B", "HR", "RBI", "SB", 
     "CS", "BB", "SO", "batting_avg", "onbase_perc", "slugging_perc", "onbase_plus_slugging", 
