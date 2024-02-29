3c88773195ed9d1852fbdb49f44e4ea05b4982e7
“Vijai <vijai.p.rajpal@gmail.com>
2024-02-29 11:54:56 -0500

    soup for teams

--- generate_teams.py
+++ generate_teams.py
@@ -1,2 +1,17 @@
-
 # generate teams
+# A script that populates the team table in scrape_mlb
+
+import requests
+from bs4 import BeautifulSoup
+from dotenv import dotenv_values
+
+config = dotenv_values(".env") 
+
+
+page = requests.get("https://www.baseball-reference.com/teams/")
+
+soup = BeautifulSoup(page.content, "lxml")
+
+all_active_teams = soup.find("table", {"id": "teams_active"})
+
+print(all_active_teams)
